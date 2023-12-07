887b7affc63a95440b266f898fdb7a7a4b5f77dc
zhi <fang.zway@gmail.com>
2023-12-07 14:47:51 -0500

    section 3 completed

--- main.go
+++ main.go
@@ -2,47 +2,77 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1> Contact Page  </h1> <p> To get in touch, email me at <a href=\"mailto:test@test.com\"> test@test.com</a>. </p>")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
+}
+
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-  <ul>
-	<li>
-	  <b>Is there a free version?</b>
-	  Yes! We offer a free trial for 30 days on any paid plans.
-	</li>
-	<li>
-	  <b>What are your support hours?</b>
-	  We have support staff answering emails 24/7, though response
-	  times may be a bit slower on weekends.
-	</li>
-	<li>
-	  <b>How do I contact support?</b>
-	  Email us - <a href="mailto:support@test.com">support@test.com</a>
-	</li>
-  </ul>
-  `)
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		log.Printf("processing template: %v", err)
+		http.Error(w, "There was an error processing the template.", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "There was an errore executing the template.", http.StatusInternalServerError)
+		return
+	}
+}
+
+func GalleriesCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		galleriesID := chi.URLParam(r, "galleriesID")
+		fmt.Println(galleriesID)
+	})
+}
+
+func getGalleries(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Galleries")
 }
 
 func main() {
 	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.Route("/galleries", func(r chi.Router) {
+		r.Get("/", getGalleries)
+		// Subrouters:
+		r.Route("/{galleriesID}", func(r chi.Router) {
+			r.Use(GalleriesCtx)
+			r.Get("/", getGalleries)
+		})
+	})
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
