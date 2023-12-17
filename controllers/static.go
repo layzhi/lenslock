50be3f9589041dc2cf46726fdf7a55a729b59a0a
zhi <fang.zway@gmail.com>
2023-12-16 23:47:16 -0500

    section_7

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"lenslocked/views"
 	"net/http"
 )
@@ -16,5 +17,24 @@ func (static Static) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any plan.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "Our support hours are monday - friday, 9 am - 5 pm EST",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
