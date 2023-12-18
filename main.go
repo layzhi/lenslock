4246b257ef9103d2431eb5fbcff96e85182b91b0
zhi <fang.zway@gmail.com>
2023-12-17 23:26:13 -0500

    initial commit for section 8

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ func main() {
 	r.Use(middleware.Recoverer)
 
 	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
+	r.Get("/signup", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))))
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
